691cb32d08fa5377d6cbc1a74ddebeee5457eb35
Anton Vitushko <vitushko.anton@gmail.com>
2022-05-22 15:32:07 +0300

    My solution v0.1

---
+++ go.mod
@@ -0,0 +1,3 @@
+module golang-united-school-homework-4.1
+
+go 1.17
--- reverse_string.go
+++ reverse_string.go
@@ -1,6 +1,12 @@
 package reverse_string
 
+import "fmt"
+
 func ReverseString(input string) (output string) {
 	// solution goes here
+
+	for _, name := range input {
+		output = fmt.Sprint(string(name)) + output
+	}
 	return output
 }
